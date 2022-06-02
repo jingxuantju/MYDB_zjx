fd4246a8c1c4ad253349e24f86e5e5f98e1ac4d1
zjx <292430178@qq.com>
2022-06-02 19:42:47 +0800

    1

---
+++ src/main/java/backend/tm/TransactionManagerImpl.java
@@ -0,0 +1,105 @@
+package backend.tm;
+
+import backend.utils.Panic;
+import backend.utils.Parser;
+import common.Error;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.concurrent.locks.Lock;
+
+public class TransactionManagerImpl implements TransactionManager {
+
+    // XID文件头长度
+    static final int LEN_XID_HEADER_LENGTH = 8;
+    // 每个事务的占用长度
+    private static final int XID_FIELD_SIZE = 1;
+
+    // 事务的三种状态
+    private static final byte FIELD_TRAN_ACTIVE   = 0;
+    private static final byte FIELD_TRAN_COMMITTED = 1;
+    private static final byte FIELD_TRAN_ABORTED  = 2;
+
+    // 超级事务，永远为commited状态
+    public static final long SUPER_XID = 0;
+
+    static final String XID_SUFFIX = ".xid";
+
+    private RandomAccessFile file;
+    private FileChannel fc;
+    private long xidCounter;
+    private Lock counterLock;
+
+    /**
+     * 检查XID文件是否合法
+     * 读取XID_FILE_HEADER中的xidcounter，根据它计算文件的理论长度，对比实际长度
+     */
+    private void checkXIDCounter() {
+        long fileLen = 0;
+        try {
+            fileLen = file.length();
+        } catch (IOException e1) {
+            Panic.panic(Error.BadXIDFileException);
+        }
+        if(fileLen < LEN_XID_HEADER_LENGTH) {
+            Panic.panic(Error.BadXIDFileException);
+        }
+
+        ByteBuffer buf = ByteBuffer.allocate(LEN_XID_HEADER_LENGTH);
+        try {
+            fc.position(0);
+            fc.read(buf);
+        } catch (IOException e) {
+            Panic.panic(e);
+        }
+        this.xidCounter = Parser.parseLong(buf.array());
+        long end = getXidPosition(this.xidCounter + 1);
+        if(end != fileLen) {
+            Panic.panic(Error.BadXIDFileException);
+        }
+    }
+
+
+
+    @Override
+    public long begin() {
+        return 0;
+    }
+
+    @Override
+    public void commit(long xid) {
+
+    }
+
+    @Override
+    public void abort(long xid) {
+
+    }
+
+    @Override
+    public boolean isActive(long xid) {
+        return false;
+    }
+
+    @Override
+    public boolean isCommitted(long xid) {
+        return false;
+    }
+
+    @Override
+    public boolean isAborted(long xid) {
+        return false;
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    // 根据事务xid取得其在xid文件中对应的位置
+    private long getXidPosition(long xid) {
+        return LEN_XID_HEADER_LENGTH + (xid-1)*XID_FIELD_SIZE;
+    }
+}
