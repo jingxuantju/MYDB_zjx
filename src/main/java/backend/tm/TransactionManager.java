24210f191a214adda4a3184ff889c5f0da329844
zjx <292430178@qq.com>
2022-06-02 20:22:07 +0800

    tm

--- src/main/java/backend/tm/TransactionManager.java
+++ src/main/java/backend/tm/TransactionManager.java
@@ -1,6 +1,15 @@
 package backend.tm;
 
 
+import backend.utils.Panic;
+import common.Error;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 
 public interface TransactionManager {
     long begin();                       // 开启一个新事务
@@ -10,4 +19,59 @@ public interface TransactionManager {
     boolean isCommitted(long xid);      // 查询一个事务的状态是否是已提交
     boolean isAborted(long xid);        // 查询一个事务的状态是否是已取消
     void close();                       // 关闭TM
+
+    public static TransactionManagerImpl create(String path) {
+        File f = new File(path+TransactionManagerImpl.XID_SUFFIX);
+        try {
+            if(!f.createNewFile()) {
+                Panic.panic(Error.FileExistsException);
+            }
+        } catch (Exception e) {
+            Panic.panic(e);
+        }
+        if(!f.canRead() || !f.canWrite()) {
+            Panic.panic(Error.FileCannotRWException);
+        }
+
+        FileChannel fc = null;
+        RandomAccessFile raf = null;
+        try {
+            raf = new RandomAccessFile(f, "rw");
+            fc = raf.getChannel();
+        } catch (FileNotFoundException e) {
+            Panic.panic(e);
+        }
+
+        // 写空XID文件头
+        ByteBuffer buf = ByteBuffer.wrap(new byte[TransactionManagerImpl.LEN_XID_HEADER_LENGTH]);
+        try {
+            fc.position(0);
+            fc.write(buf);
+        } catch (IOException e) {
+            Panic.panic(e);
+        }
+
+        return new TransactionManagerImpl(raf, fc);
+    }
+
+    public static TransactionManagerImpl open(String path) {
+        File f = new File(path+TransactionManagerImpl.XID_SUFFIX);
+        if(!f.exists()) {
+            Panic.panic(Error.FileNotExistsException);
+        }
+        if(!f.canRead() || !f.canWrite()) {
+            Panic.panic(Error.FileCannotRWException);
+        }
+
+        FileChannel fc = null;
+        RandomAccessFile raf = null;
+        try {
+            raf = new RandomAccessFile(f, "rw");
+            fc = raf.getChannel();
+        } catch (FileNotFoundException e) {
+            Panic.panic(e);
+        }
+
+        return new TransactionManagerImpl(raf, fc);
+    }
 }
--- src/main/java/backend/tm/TransactionManagerImpl.java
+++ src/main/java/backend/tm/TransactionManagerImpl.java
@@ -9,6 +9,7 @@ import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class TransactionManagerImpl implements TransactionManager {
 
@@ -18,9 +19,9 @@ public class TransactionManagerImpl implements TransactionManager {
     private static final int XID_FIELD_SIZE = 1;
 
     // 事务的三种状态
-    private static final byte FIELD_TRAN_ACTIVE   = 0;
+    private static final byte FIELD_TRAN_ACTIVE = 0;
     private static final byte FIELD_TRAN_COMMITTED = 1;
-    private static final byte FIELD_TRAN_ABORTED  = 2;
+    private static final byte FIELD_TRAN_ABORTED = 2;
 
     // 超级事务，永远为commited状态
     public static final long SUPER_XID = 0;
@@ -31,6 +32,13 @@ public class TransactionManagerImpl implements TransactionManager {
     private FileChannel fc;
     private long xidCounter;
     private Lock counterLock;
+
+    TransactionManagerImpl(RandomAccessFile raf, FileChannel fc) {
+        this.file = raf;
+        this.fc = fc;
+        counterLock = new ReentrantLock();
+        checkXIDCounter();
+    }
 
     /**
      * 检查XID文件是否合法
@@ -43,7 +51,7 @@ public class TransactionManagerImpl implements TransactionManager {
         } catch (IOException e1) {
             Panic.panic(Error.BadXIDFileException);
         }
-        if(fileLen < LEN_XID_HEADER_LENGTH) {
+        if (fileLen < LEN_XID_HEADER_LENGTH) {
             Panic.panic(Error.BadXIDFileException);
         }
 
@@ -56,50 +64,111 @@ public class TransactionManagerImpl implements TransactionManager {
         }
         this.xidCounter = Parser.parseLong(buf.array());
         long end = getXidPosition(this.xidCounter + 1);
-        if(end != fileLen) {
+        if (end != fileLen) {
             Panic.panic(Error.BadXIDFileException);
         }
     }
 
 
-
-    @Override
-    public long begin() {
-        return 0;
-    }
-
-    @Override
-    public void commit(long xid) {
-
-    }
-
-    @Override
-    public void abort(long xid) {
-
-    }
-
-    @Override
-    public boolean isActive(long xid) {
-        return false;
-    }
-
-    @Override
-    public boolean isCommitted(long xid) {
-        return false;
-    }
-
-    @Override
-    public boolean isAborted(long xid) {
-        return false;
-    }
-
-    @Override
-    public void close() {
-
-    }
-
     // 根据事务xid取得其在xid文件中对应的位置
     private long getXidPosition(long xid) {
-        return LEN_XID_HEADER_LENGTH + (xid-1)*XID_FIELD_SIZE;
+        return LEN_XID_HEADER_LENGTH + (xid - 1) * XID_FIELD_SIZE;
     }
+
+    // 更新xid事务的状态为status
+    private void updateXID(long xid, byte status) {
+        long offset = getXidPosition(xid);
+        byte[] tmp = new byte[XID_FIELD_SIZE];
+        tmp[0] = status;
+        ByteBuffer buf = ByteBuffer.wrap(tmp);
+        try {
+            fc.position(offset);
+            fc.write(buf);
+        } catch (IOException e) {
+            Panic.panic(e);
+        }
+        try {
+            fc.force(false);
+        } catch (IOException e) {
+            Panic.panic(e);
+        }
+    }
+
+    // 将XID加一，并更新XID Header
+    private void incrXIDCounter() {
+        xidCounter++;
+        ByteBuffer buf = ByteBuffer.wrap(Parser.long2Byte(xidCounter));
+        try {
+            fc.position(0);
+            fc.write(buf);
+        } catch (IOException e) {
+            Panic.panic(e);
+        }
+        try {
+            fc.force(false);
+        } catch (IOException e) {
+            Panic.panic(e);
+        }
+    }
+
+    // 开始一个事务，并返回XID
+    public long begin() {
+        counterLock.lock();
+        try {
+            long xid = xidCounter + 1;
+            updateXID(xid, FIELD_TRAN_ACTIVE);
+            incrXIDCounter();
+            return xid;
+        } finally {
+            counterLock.unlock();
+        }
+    }
+
+    // 提交XID事务
+    public void commit(long xid) {
+        updateXID(xid, FIELD_TRAN_COMMITTED);
+    }
+
+    // 回滚XID事务
+    public void abort(long xid) {
+        updateXID(xid, FIELD_TRAN_ABORTED);
+    }
+
+    // 检测XID事务是否处于status状态
+    private boolean checkXID(long xid, byte status) {
+        long offset = getXidPosition(xid);
+        ByteBuffer buf = ByteBuffer.wrap(new byte[XID_FIELD_SIZE]);
+        try {
+            fc.position(offset);
+            fc.read(buf);
+        } catch (IOException e) {
+            Panic.panic(e);
+        }
+        return buf.array()[0] == status;
+    }
+
+    public boolean isActive(long xid) {
+        if (xid == SUPER_XID) return false;
+        return checkXID(xid, FIELD_TRAN_ACTIVE);
+    }
+
+    public boolean isCommitted(long xid) {
+        if (xid == SUPER_XID) return true;
+        return checkXID(xid, FIELD_TRAN_COMMITTED);
+    }
+
+    public boolean isAborted(long xid) {
+        if (xid == SUPER_XID) return false;
+        return checkXID(xid, FIELD_TRAN_ABORTED);
+    }
+
+    public void close() {
+        try {
+            fc.close();
+            file.close();
+        } catch (IOException e) {
+            Panic.panic(e);
+        }
+    }
+
 }
